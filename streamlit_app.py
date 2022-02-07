0d66f143adb258fd0b14e12cf14e9021bb595fa8
Elie Merhej <elie.merhej@kingmakers.com>
2022-02-07 13:27:28 +0400

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
-import pyspark.sql
-from pyspark.dbutils import DBUtils
+# import pyspark.sql
+# from pyspark.dbutils import DBUtils
 
 
 #***********************************************************************************************************************
 #***********************************************************************************************************************
 st.title('Customer Profiling')
 
-spark = pyspark.sql.SparkSession.builder.getOrCreate()
-dbutils = DBUtils(spark)
-
-st.write(dbutils.fs.ls('dbfs:/elie'))
+# spark = pyspark.sql.SparkSession.builder.getOrCreate()
+# dbutils = DBUtils(spark)
+#
+# st.write(dbutils.fs.ls('dbfs:/elie'))
