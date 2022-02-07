701efbf1be24e84d328d250d483c671f7daa47fa
Elie Merhej <elie.merhej@kingmakers.com>
2022-02-07 13:50:01 +0400

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,21 @@
 import streamlit as st
+
+from databricks_api import DatabricksAPI
+
 # import pyspark.sql
 # from pyspark.dbutils import DBUtils
 
+db = DatabricksAPI(
+    host="adb-2820452106200483.3.azuredatabricks.net"
+)
 
 #***********************************************************************************************************************
-#***********************************************************************************************************************
+#******************************************£*****************************************************************************
 st.title('Customer Profiling')
+
+
+st.write(db.dbfs.list('dbfs:/elie'))
+
 
 # spark = pyspark.sql.SparkSession.builder.getOrCreate()
 # dbutils = DBUtils(spark)
