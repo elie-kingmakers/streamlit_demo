15386e7044f2d08504abbc86f16b693c69454239
Elie Merhej <elie.merhej@kingmakers.com>
2022-02-07 14:42:01 +0400

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,27 @@
+
 import streamlit as st
 
 from databricks_api import DatabricksAPI
-
-# import pyspark.sql
-# from pyspark.dbutils import DBUtils
+import pyspark.sql
+from pyspark.dbutils import DBUtils
+import requests
+import json
 
 db = DatabricksAPI(
     host="adb-2820452106200483.3.azuredatabricks.net",
     token="dapi0f9b9050d21ecca66a72fe2b1155ad5e-2"
 )
+#
+# spark = pyspark.sql.SparkSession.builder.getOrCreate()
+# dbutils = DBUtils(spark)
 
 #***********************************************************************************************************************
 #***********************************************************************************************************************
 st.title('Customer Profiling')
 
 
-st.write(db.dbfs.list('dbfs:/elie'))
+st.write(db.dbfs.list('dbfs:/mnt/datascience/customer_profiling/gold/customers'))
 
 
-# spark = pyspark.sql.SparkSession.builder.getOrCreate()
-# dbutils = DBUtils(spark)
-#
-# st.write(dbutils.fs.ls('dbfs:/elie'))
+
+
