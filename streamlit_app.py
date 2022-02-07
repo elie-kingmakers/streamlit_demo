46ee783c1a7238d3ec00bb232a2615284413886b
Elie Merhej <elie.merhej@kingmakers.com>
2022-02-07 14:42:32 +0400

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@
 import streamlit as st
 
 from databricks_api import DatabricksAPI
-import pyspark.sql
-from pyspark.dbutils import DBUtils
-import requests
-import json
+# import pyspark.sql
+# from pyspark.dbutils import DBUtils
+# import requests
+# import json
 
 db = DatabricksAPI(
     host="adb-2820452106200483.3.azuredatabricks.net",
