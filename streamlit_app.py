85629f9d7f71bf4681525cdb283d5d8595eaad10
Elie Merhej <elie.merhej@kingmakers.com>
2022-06-13 15:04:22 +0400

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,7 +151,7 @@ if getProfileButton or (userPlatformId != DEFAULT_PLATFORM_USER_ID):
     )
 
     col1.metric(label="Highest Coupon Stake", value="{:.2f}".format(customerProfile.highestCouponStake))
-    col2.metric(label="Highest Coupon Return", value="{:.2f}".format(customerProfile.highestCouponReturn))
+    col2.metric(label="Highest Coupon Return", value="{:.2f}".format(-customerProfile.highestCouponReturn))
 
     # col1.metric(label='Avg. Probability Estimate KC', value='{:.2f}'.format(customerProfile.averageProbabilityEstimateKellyCriterion*100.0))
     # col2.metric(label='Avg. Bet Score', value='{:.2f}'.format(customerProfile.averageBetScore))
