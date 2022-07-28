a3b431bb9aa3da031c1921763c950c9868c62aef
Elie Merhej <elie.merhej@kingmakers.com>
2022-07-28 12:18:13 +0400

    removed pyspark dependency

--- core/utils/matplotlib_helpers.py
+++ core/utils/matplotlib_helpers.py
@@ -3,34 +3,6 @@ from typing import Optional, List
 import matplotlib.pyplot as plt
 import textwrap
 
-import pyspark.sql
-
-
-def get_histogram(
-        df: pyspark.sql.DataFrame,
-        columnName: str,
-        xLabel: str = "",
-        yLabel: str = "",
-        binsNum: int = 100,
-        xScale: str = "linear",
-        yScale: str = "linear",
-        xTicks: Optional[List[float]] = None,
-) -> plt.Figure:
-    fig, ax = plt.subplots()
-
-    bins, counts = df.select(columnName).rdd.flatMap(lambda x: x).histogram(binsNum)
-    ax.hist(bins[:-1], bins=bins, weights=counts)
-
-    ax.set_xlabel(xLabel)
-    ax.set_ylabel(yLabel)
-    ax.set_xscale(xScale)
-    ax.set_yscale(yScale)
-
-    if xTicks is not None:
-        ax.set_xticks(xTicks)
-
-    return fig
-
 
 def wrap_labels(ax, width, break_long_words=False):
     labels = []
